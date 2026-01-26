4c6691b33c8e7ad5f8ff4416b22c46b0ecaa23e6
Mhirax <olowookeremiracle14@gmail.com>
2026-01-26 14:35:56 +0100

    IMPORT and EXPORT my game component

--- ignite/src/components/Game.js
+++ ignite/src/components/Game.js
@@ -9,3 +9,5 @@ const Game = () => {
         </div>
     )
 }
+
+export default Game;
--- ignite/src/pages/Home.js
+++ ignite/src/pages/Home.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 //Redux
 import { useDispatch } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
+//components
+import Game from '../components/Game';
 
 const Home = () => {
   // FETCH GAMES
