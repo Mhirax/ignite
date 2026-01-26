6afa10dfbbb8ac586f68fcb94d684791fa84aa2c
Mhirax <olowookeremiracle14@gmail.com>
2026-01-26 16:01:48 +0100

    styling my games

--- ignite/src/pages/Home.js
+++ ignite/src/pages/Home.js
@@ -1,13 +1,12 @@
 import React, { useEffect } from "react";
 //Redux
-import { useDispatch,useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
 //components
-import Game from '../components/Game';
+import Game from "../components/Game";
 //Styling and Animation
-import styled from 'styled-components';
-import { motion } from 'framer-motion';
-
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Home = () => {
   // FETCH GAMES
@@ -15,30 +14,37 @@ const Home = () => {
   useEffect(() => {
     dispatch(loadGames());
   }, [dispatch]);
-    //Get the data back
-    const { popular, newGames, upcoming} = useSelector((state) => state.games);
-  
+  //Get the data back
+  const { popular, newGames, upcoming } = useSelector((state) => state.games);
+
   return (
     <GameList>
-          <h1>Upcoming Games</h1>
-          <Games>
-              {upcoming.map((game) => (<Game
-                  name={game.name}
-                  released={game.released}
-                  id={game.id}
-                  image={game.background_image}
-                  key={game.id}
-              />
-              ))}
-          </Games>
+      <h1>Upcoming Games</h1>
+      <Games>
+        {upcoming.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
     </GameList>
   );
 };
 
-const GameList = styled(motion.div)`   
-
-`
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
 const Games = styled(motion.div)`
-`
+min-height: 80vh;
+display: grid;
+grid-template-columns: repeat(auto-fit, minmax(500px,1fr));
+`;
 
 export default Home;
