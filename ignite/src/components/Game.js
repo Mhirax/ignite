22aa7069b7f1c1dec84d1802417b1a7f992d762f
Mhirax <olowookeremiracle14@gmail.com>
2026-01-26 18:24:01 +0100

    adding textalign and border radius

--- ignite/src/components/Game.js
+++ ignite/src/components/Game.js
@@ -13,7 +13,12 @@ const Game = ({ name, released, image }) => {
     );
 };
 
-const StyledGame = styled(motion.div)`img {
+const StyledGame = styled(motion.div)`
+min-height: 30vh;
+box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
+text-align: center
+border-radius: 1rem;
+img {
     width: 100%;
 };`
 
